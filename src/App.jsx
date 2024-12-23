292fc8a9da72aa67a0d548ddeee0214637d8be8f
naim-haider <naimhaider76@gmail.com>
2024-12-23 23:28:27 +0530

    installed tailwind css in the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>app</>;
+  return (
+    <>
+      <h1 className="text-blue-700 font-bold  text-normal">
+        Welcome to Tailwind CSS Installation Tutorial.
+      </h1>
+    </>
+  );
 }
 
 export default App;
