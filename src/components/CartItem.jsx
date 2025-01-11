ac594d11688cf8ced3252c138242355687204a91
naim-haider <naimhaider76@gmail.com>
2025-01-11 13:12:30 +0530

    removed some comments

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -124,42 +124,6 @@ const CartItem = ({ item }) => {
           </div>
         </div>
       </div>
-      {/* <div className="flex flex-col md:flex-row items-center md:items-center justify-between lg:px-6 pb-6 border-b border-gray-200 max-lg:max-w-lg max-lg:mx-auto">
-            <h5 className="text-gray-900 font-manrope font-semibold text-2xl leading-9 w-full max-md:text-center max-md:mb-4">
-              Subtotal
-            </h5>
-            <div className="flex items-center justify-between gap-5 ">
-              <button className="rounded-full py-2.5 px-3 bg-indigo-50 text-indigo-600 font-semibold text-xs text-center whitespace-nowrap transition-all duration-500 hover:bg-indigo-100">
-                Promo Code?
-              </button>
-              <h6 className="font-manrope font-bold text-3xl lead-10 text-indigo-600">
-                $440
-              </h6>
-            </div>
-          </div>
-          <div className="max-lg:max-w-lg max-lg:mx-auto">
-            <p className="font-normal text-base leading-7 text-gray-500 text-center mb-5 mt-6">
-              Shipping taxes, and discounts calculated at checkout
-            </p>
-            <button className="rounded-full py-4 px-6 bg-indigo-600 text-white font-semibold text-lg w-full text-center transition-all duration-500 hover:bg-indigo-700 ">
-              Checkout
-            </button>
-          </div>
-        </div>
-      </section> */}
-
-      {/*  */}
-      {/*  */}
-      {/* <div className="cart-item">
-        <h4>{item.name}</h4>
-        <p>Price: ${item.price}</p>
-        <div className="quantity-controls">
-          <button onClick={handleDecrease}>-</button>
-          <span>{item.quantity}</span>
-          <button onClick={handleIncrease}>+</button>
-        </div>
-        <button onClick={handleRemove}>Remove</button>
-      </div> */}
     </>
   );
 };
