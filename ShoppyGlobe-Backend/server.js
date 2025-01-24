5d3a0e3d8a1d79471a88ce81eecd415f677d8c0d
naim-haider <naimhaider76@gmail.com>
2025-01-25 00:26:56 +0530

    connected to mongodb

--- ShoppyGlobe-Backend/server.js
+++ ShoppyGlobe-Backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import connectDB from "./database/db.js";
 
 const app = express();
+connectDB();
 
 app.use(express.json()); // to parse JSON bodies
 
-const PORT = 5100;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
