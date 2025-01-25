19f943b6be84b1e7c5eac29c683bb0379aced264
naim-haider <naimhaider76@gmail.com>
2025-01-25 22:46:43 +0530

    created admin and user protected middleware, created admin controller to add a new product, update and delete the existing products

--- ShoppyGlobe-Backend/server.js
+++ ShoppyGlobe-Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./database/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/UserRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 connectDB();
@@ -11,6 +12,7 @@ app.use(express.json()); // to parse JSON bodies
 // Routes
 app.use("/api", productRoutes);
 app.use("/api", userRoutes);
+app.use("/api", adminRoutes);
 
 const PORT = 5000;
 
