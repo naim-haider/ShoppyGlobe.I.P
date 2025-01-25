a9150ba629d442d5ef418a907d2a1718ffa02e17
naim-haider <naimhaider76@gmail.com>
2025-01-25 15:32:43 +0530

    created userController and loginUser and registerUser routes, used jwt-token

--- ShoppyGlobe-Backend/server.js
+++ ShoppyGlobe-Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./database/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/UserRoutes.js";
 
 const app = express();
 connectDB();
@@ -9,6 +10,7 @@ app.use(express.json()); // to parse JSON bodies
 
 // Routes
 app.use("/api", productRoutes);
+app.use("/api", userRoutes);
 
 const PORT = 5000;
 
