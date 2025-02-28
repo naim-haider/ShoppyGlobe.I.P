cd7f266e674efaf7cd7e5db82d4a4c331ae48331
naim-haider <naimhaider76@gmail.com>
2025-02-28 21:18:46 +0530

    added vercel file

--- ShoppyGlobe-Backend/server.js
+++ ShoppyGlobe-Backend/server.js
@@ -5,6 +5,9 @@ import userRoutes from "./routes/UserRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 connectDB();
 const app = express();
@@ -18,8 +21,11 @@ app.use("/api", productRoutes);
 app.use("/api", cartRoutes);
 app.use("/api", userRoutes);
 app.use("/api", adminRoutes);
+app.get("/", (req, res) => {
+  res.send("root route");
+});
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
